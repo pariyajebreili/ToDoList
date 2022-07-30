1e3dca9d3abdce563d44ff171e0426b94277e885
pariya <pariyajebreili@gmail.com>
2022-07-30 18:11:57 +0430

    changes in models

--- base/models.py
+++ base/models.py
@@ -5,8 +5,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Task(models.Model):
-    user =  
-    title = 
-    description =
-    complete = 
-    created =
+    user =  models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False) 
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    class Meta: 
+        ordering = ['complete']
