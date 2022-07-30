5d13de285f2f45b1ac27e51395cbdf7c7d20daf3
Pariya <87968419+pariyajebreili@users.noreply.github.com>
2022-07-30 17:51:27 +0430

    Add files via upload

---
+++ base/models.py
@@ -0,0 +1,12 @@
+from sqlite3 import complete_statement
+from venv import create
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Task(models.Model):
+    user =  
+    title = 
+    description =
+    complete = 
+    created =
