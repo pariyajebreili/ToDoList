32938dea546c78cd63a8a47aae0d4b1bd17657f3
pariya <pariyajebreili@gmail.com>
2022-08-01 21:34:11 +0430

    add register page

--- base/views.py
+++ base/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from .models import Task
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 class CustomLoginView(LoginView):
         template_name: 'registration/login.html'
@@ -16,6 +17,11 @@ class CustomLoginView(LoginView):
             return reverse_lazy('tasks')
 
 
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
 
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
